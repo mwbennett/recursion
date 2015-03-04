228419e466eef660f267e0f73c0c2a54464f19ef
Mark Bennett <mwbennett@wesleyan.edu>
2015-03-03 18:25:49 -0800

    Updated variable names; implemented handling for arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,16 +3,35 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(element) {
-  if (typeof element === 'function' || element === undefined){
-    return;
+var stringifyJSON = function(input) {
+  
+  // stringify non-object arguments correctly 
+  if (typeof input === 'function' || input === undefined){
+    return undefined;
   }
-  if (element === null){
+  if (input === null){
     return 'null';
   }
-  if (typeof element === 'string'){
-    return '"' + element + '"';
-  } else if (typeof element !== 'object'){
-    return element.toString();
+  if (typeof input === 'string'){
+    return '"' + input + '"';
+  } else if (typeof input !== 'object'){
+    return input.toString();
+  }
+  // recursively stringify arrays
+  if (Array.isArray(input)){
+    var stringyArray = '[';
+    for (var i = 0; i < input.length; i++){
+      var element = input[i];
+      if (typeof element === 'number' || typeof element === 'boolean' || element === null){
+        stringyArray = stringyArray + element;
+      }else {
+        stringyArray = stringyArray + stringifyJSON(element);
+      }
+      if (i < input.length - 1){
+        stringyArray = stringyArray + ',';
+      }
+    }
+    stringyArray = stringyArray + ']';
+    return stringyArray;
   }
 };
