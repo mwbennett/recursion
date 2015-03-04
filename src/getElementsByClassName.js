71cfd8dc4d5b061ff8e47024dbaaa7c7c613b5c9
Mark Bennett <mwbennett@wesleyan.edu>
2015-03-03 21:35:31 -0800

    Imlemented getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  var $body = document.body;
+  var result = [];
+  
+  var test = function(element){
+    var nodeClasses = element.classList;
+    if(nodeClasses && nodeClasses.contains(className)){
+      result.push(element);
+    }
+  };
+
+  var traverse = function(node, func){
+    func(node);
+    node = node.firstChild;
+    while (node){
+      traverse(node, func);
+      node = node.nextSibling;
+    }
+  }
+
+  traverse($body, test);
+  return result;
 };
